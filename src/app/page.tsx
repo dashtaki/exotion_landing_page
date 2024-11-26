27fab9cc458aa7d4e08bc0bb1e9a1428cd41986c
Mohammad Jamal Dashtaki <m.dashtaki@epilot.cloud>
2024-11-26 21:10:36 +0100

    Fix screenshots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,34 +7,34 @@ import CookieBanner from '@/components/CookieBanner'
 
 const SECTIONS: SectionProps[] = [
   {
-    title: 'Ask for a challenge',
-    description: 'Post unique challenges for others to complete, like experiencing a café in Paris.',
-    images: ['/Home.png', '/GeneralProfile.png'],
+    title: 'Create a challenge',
+    description: 'Create a challenge centered around an experience you’d love to have and share it with others who might want to try it. For instance, sipping coffee while enjoying the stunning view of the Eiffel Tower. Let your ideas spark adventures for others!',
+    images: ['/CreateChallenge.png', '/ChallengeList.png'],
     rtl: true,
     index: 0
   },
   {
     title: 'Share your experience',
-    description: 'Complete challenges and share your journey through video submissions.',
-    images: ['/AboutChallenge.png', '/ChallengePreview.png'],
+    description: 'Share your experiences with a wider audience and let your experience inspire and resonate with others. Sharing allows you to connect with people who value and admire it. Watch as your experiences spark conversations, gather likes, and create a ripple of engagement that brings people together.',
+    images: ['/ChallengePreview.png', '/AboutChallenge.png'],
     rtl: false,
     index: 1
   },
   {
-    title: 'Be a challenger',
-    description: 'Take on exciting tasks, complete them, and showcase your adventurous spirit.',
-    images: ['/Home.png', '/GeneralProfile.png'],
+    title: 'Make money',
+    description: 'Earn compensation for completing and sharing approved challenges.',
+    images: ['/Dashboard.png', '/ChallengePreview.png'],
     rtl: true,
     index: 2
   },
   {
-    title: 'Make money',
-    description: 'Earn compensation for completing and sharing approved challenges.',
-    images: ['/AboutChallenge.png', '/ChallengePreview.png'],
+    title: 'Be a challenger',
+    description: 'Take on exciting tasks, complete them, and showcase your adventurous spirit.',
+    images: ['/ChallengeDetail.png', '/ChallengeCompleted.png'],
     rtl: false,
     index: 3
   }
-];
+]
 
 export default function Home() {
   return (
