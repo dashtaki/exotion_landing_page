d5b371f1d235af93648e56a6d0f938b2ec3bc1e3
Mohammad Jamal Dashtaki <m.dashtaki@epilot.cloud>
2025-01-21 17:37:09 +0100

    New Design: Wrap up titles

--- src/contants.ts
+++ src/contants.ts
@@ -10,33 +10,23 @@ export const SECTIONS: SectionProps[] = [
       'Create a challenge centered around an experience you’d love to have and share it with others who might want to try it. For instance, sipping coffee while enjoying the stunning view of the Eiffel Tower. Let your ideas spark adventures for others!',
   },
   {
-    title: 'Accept a challenge',
-    description:
-      'Create a challenge centered around an experience you’d love to have and share it with others who might want to try it. For instance, sipping coffee while enjoying the stunning view of the Eiffel Tower. Let your ideas spark adventures for others!',
-  },
-  {
-    title: 'Share your experience',
-    description:
-      'Share your experiences with a wider audience and let your experience inspire and resonate with others. Sharing allows you to connect with people who value and admire it. Watch as your experiences spark conversations, gather likes, and create a ripple of engagement that brings people together.',
-  },
-  {
-    title: 'Share your experience',
-    description:
-      'Share your experiences with a wider audience and let your experience inspire and resonate with others. Sharing allows you to connect with people who value and admire it. Watch as your experiences spark conversations, gather likes, and create a ripple of engagement that brings people together.',
-  },
-  {
     title: 'Profit from Your Adventures',
     description:
       'Earn money by designing exciting challenges based on your experiences and sharing them with others. When challengers take on and complete your tasks, and their submissions are approved, you receive compensation. It’s a rewarding way to inspire others while profiting from your creativity!',
   },
   {
-    title: 'Step up and Be a challenger',
+    title: 'Inspire with your experiences',
     description:
-      'Step into the role of a challenger by taking on thrilling tasks, completing them, and showcasing your adventurous side. Not only will you enjoy unique experiences, but you’ll also have the chance to earn money as a reward for your achievements.',
+      'Connect with people who value and admire your creativity. Spark conversations, gather likes, and build a community that celebrates meaningful moments. Whether it’s street food adventures or unique challenges, your experiences have the power to engage and unite.\n',
   },
   {
     title: 'Step up and Be a challenger',
     description:
       'Step into the role of a challenger by taking on thrilling tasks, completing them, and showcasing your adventurous side. Not only will you enjoy unique experiences, but you’ll also have the chance to earn money as a reward for your achievements.',
+  },
+  {
+    title: 'Share your experience',
+    description:
+      'Take on exciting challenges and turn your creativity into cash! make money based on the impressions and view count your submissions generate and get rewarded for your impact!',
   },
 ]
